335d156017e619a37e8e1ab94ee74da22ca6b45d
Christopher Tobiasz <tobiasz.christopher@gmail.com>
2019-01-25 17:50:28 -0500

    we added a form to add users

--- server.js
+++ server.js
@@ -7,9 +7,28 @@ app.set('view engine', 'ejs');
 
 // app.[VERB]([PATH], function(req, res) [ // things to do ])
 
-
 app.get('/', (req, res) => {
   res.render('home.ejs')
+})
+
+app.get('/signup', (req, res) => {
+  res.render('users/new')
+})
+
+app.get('/users', (req, res) => {
+  res.render('users/index')
+})
+
+app.post('/signup', (req, res) => {
+  res.redirect('users/index') // redirect takes the path not the file path
+})
+
+app.get('/new-cohort', (req, res) => {
+  res.render('cohorts/new')
+})
+
+app.get('/new-course', (req, res) => {
+  res.render('courses/new')
 })
 
 app.listen(port, () => { console.log(`Express App listening on http://localhost:${port}`)})
